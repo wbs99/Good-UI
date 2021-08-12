30711b2209c8694100ccf7a2a1867cc2c462f375
wbs <1134954328@qq.com>
2021-08-12 13:19:07 +0800

    完善 aside 切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 </template>
 
 <script lang="ts">
-import {provide, ref} from 'vue'
+import {provide, ref} from 'vue';
+
 export default {
   name: 'App',
-  setup(){
-    const asideVisible= ref(false)
-    provide('asideVisible',asideVisible) //取名为 xxx ，内容就是 asideVisible
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const asideVisible = ref(width <= 500 ? false : true);
+    provide('asideVisible', asideVisible); //取名为 xxx ，内容就是 asideVisible
   }
-}
+};
 </script>
