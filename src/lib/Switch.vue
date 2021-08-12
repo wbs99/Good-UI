ac5d4c2768147d461dfe81ca745b063096e6c164
wbs <1134954328@qq.com>
2021-08-12 15:26:22 +0800

    完成 switch css

---
+++ src/lib/Switch.vue
@@ -0,0 +1,27 @@
+<template>
+  <button><span></span></button>
+</template>
+
+<script lang='ts'>
+
+</script>
+
+<style lang='scss' scoped>
+  button{
+    background: lightpink;
+    height: 22px;
+    width: 44px;
+    border-radius: 11px;
+    border:none;
+    position: relative;
+    span{
+      position: absolute;
+      top:2px;
+      left: 2px;
+      width: 18px;
+      height: 18px;
+      background: white;
+      border-radius:12px;
+    }
+  }
+</style>
