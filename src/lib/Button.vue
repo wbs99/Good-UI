ac52921b02ac4461ca237c892ced56ef23632421
wbs <1134954328@qq.com>
2021-08-12 22:56:50 +0800

    完成基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="gulu-button"
-          :class="`theme-${theme}`">
+          :class="{[`theme-${theme}`]:theme}">
     <slot/>
   </button>
 </template>
@@ -17,7 +17,32 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-div {
-  border: 1px solid red;
+.gulu-button {
+  box-sizing: border-box;
+  height: 32px;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: #333;
+  border: 1px solid #d9d9d9;
+  border-radius: 4px;
+  box-shadow: 0 1px 0 fade-out(black, 0.95) & + & {
+    margin-left: 8px;
+  }
+
+  &:hover, &:focus {
+    color: #40a9ff;
+    border-color: #40a9ff;
+  }
+  &:focus{
+    outline: none;
+  }
+  &::-moz-focus-inner{
+    border:0;
+  }
 }
 </style>
