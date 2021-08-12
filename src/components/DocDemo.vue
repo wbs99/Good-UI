bb4ca1248d12e6dca4357e10a4885e70856b3b22
wbs <1134954328@qq.com>
2021-08-12 14:56:46 +0800

    基本完成官网框架

---
+++ src/components/DocDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>doc 默认页面</p>
+  </div>
+</template>
+
+<script lang='ts'>
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/components/TabsDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>tabs</p>
+  </div>
+</template>
+
+<script lang='ts'>
+
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
