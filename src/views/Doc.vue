9c4c947c983a678b800f065c4fad86a1cb089326
wbs <1134954328@qq.com>
2021-08-12 10:54:09 +0800

    更新一下 aside 的 css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -33,5 +33,21 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+aside {
+  background: lightsalmon;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom:4px;
+  }
 
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
