2daa5231dea5c658c8193ec19852c27f5a78c7de
wbs <1134954328@qq.com>
2021-08-12 14:27:30 +0800

    路由切换之后（手机上）关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,19 @@
 
 <script lang="ts">
 import {provide, ref} from 'vue';
+import { router } from './router'
 
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
-    const asideVisible = ref(width <= 500 ? false : true);
+    const asideVisible = ref(width > 500);
     provide('asideVisible', asideVisible); //取名为 xxx ，内容就是 asideVisible
+    router.afterEach(()=>{
+      if(width<=500){
+        asideVisible.value=false
+      }
+    })
   }
 };
 </script>
---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Switch 文档</div>
+</template>
+
+<script lang='ts'>
+</script>
+
+<style lang='scss' scoped>
+
+</style>
