3c5eab70238cae0decb31cf1ec06198b61a728b8
wbs <1134954328@qq.com>
2021-08-12 15:34:21 +0800

    hover 之后白色圆圈向右滚

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -7,21 +7,26 @@
 </script>
 
 <style lang='scss' scoped>
-  button{
-    background: lightpink;
-    height: 22px;
-    width: 44px;
-    border-radius: 11px;
-    border:none;
-    position: relative;
-    span{
-      position: absolute;
-      top:2px;
-      left: 2px;
-      width: 18px;
-      height: 18px;
-      background: white;
-      border-radius:12px;
-    }
+button {
+  background: lightpink;
+  height: 22px;
+  width: 44px;
+  border-radius: 12px;
+  border: none;
+  position: relative;
+
+  span {
+    position: absolute;
+    top: 1.5px;
+    left: 2px;
+    width: 18px;
+    height: 18px;
+    background: white;
+    border-radius: 12px;
   }
+}
+
+button:hover > span {
+  left: calc(100% - 20px);
+}
 </style>
