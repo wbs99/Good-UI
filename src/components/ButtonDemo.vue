2f81aff80f5f9cf2ab3f1a66f9d3d2f252d538b5
wbs <1134954328@qq.com>
2021-08-12 22:40:49 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,13 +4,23 @@
   </div>
   <h1>示例</h1>
   <Button>你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="text">你好</Button>
+  <Button theme="link">你好</Button>
 </template>
 
 <script lang='ts'>
-import Button from '../lib/Button.vue'
+import Button from '../lib/Button.vue';
+
 export default {
-  components:{Button}
-}
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('你好');
+    };
+    return {onClick};
+  }
+};
 </script>
 
 <style lang='scss' scoped>
