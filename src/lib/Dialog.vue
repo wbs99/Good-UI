fc28b61ff57933bda0a750a6235f947937274155
wbs <1134954328@qq.com>
2021-08-13 20:11:35 +0800

    初步完成 dialog 的css

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="dialog-overlay">Dialog</div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>1</p>
-      <p>2</p>
-    </main>
-    <footer>
-      <Button>确认</Button>
-      <Button>取消</Button>
-    </footer>
+  <div class="good-dialog-overlay">Dialog</div>
+  <div class="good-dialog-wrapper">
+    <div class="good-dialog">
+      <header>标题 <span class="good-dialog-close"></span></header>
+      <main>
+        <p>对话框内容</p>
+        <p>对话框内容</p>
+        <p>对话框内容</p>
+      </main>
+      <footer>
+        <Button>取消</Button>
+        <Button level="main">确定</Button>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -22,5 +25,73 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.good-dialog {
+  background: white;
+  min-width: 30em;
+  max-width: 90%;
+  border-radius: 6px;
 
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade-out(black, 0.7);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+
+  > header {
+    padding: 12px 16px;
+    font-size: 18px;
+    border-bottom: 1px solid #d9d9d9;
+    border-radius: 6px 6px 0 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  > main {
+    padding: 12px 16px;
+  }
+
+  > footer {
+    border-top: 1px solid #d9d9d9;
+    padding: 12px 16px;
+    text-align: right;
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &::before, &::after {
+      content: '';
+      position: absolute;
+      background: black;
+      top: 50%;
+      left: 50%;
+      height: 1px;
+      width: 100%;
+    }
+    &::before{
+      transform: translate(-50%,-50%) rotate(-45deg);
+    }
+    &::after{
+      transform: translate(-50%,-50%) rotate(45deg);
+    }
+  }
+}
 </style>
