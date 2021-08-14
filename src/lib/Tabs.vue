8cc074a85aa4a244291106caed693d15e28a4ba7
wbs <1134954328@qq.com>
2021-08-14 22:06:53 +0800

    实现点击 nav 出现横线并伴随动画

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="good-tabs">
-    <div class="good-tabs-nav">
+    <div class="good-tabs-nav" ref="container">
       <div class="good-tabs-nav-item"
            :class="{selected:title===selected}"
            v-for="(title,index) in titles" :key="index"
+           :ref="el => { if (el) navItems[index] = el }"
            @click="select(title)">
         {{ title }}
       </div>
+      <div class="good-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="good-tabs-content">
       <component class="good-tabs-content-item" :is="current" :key="current.props.title"/>
@@ -16,7 +18,7 @@
 
 <script lang='ts'>
 import Tab from './Tab.vue';
-import {computed} from 'vue';
+import {computed, onMounted, onUpdated, ref} from 'vue';
 
 export default {
   props: {
@@ -25,6 +27,22 @@ export default {
     }
   },
   setup(props, context) {
+    //TS 并不知道下面写的 div 是什么类型，这里的意思 ref 中的数组就是 HTMLDiv元素的数组（泛型语法）
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter(div => div.classList.contains('selected'))[0];
+      const {width} = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const containerLeft = container.value.getBoundingClientRect().left;
+      const divLeft = result.getBoundingClientRect().left;
+      const left = divLeft - containerLeft;
+      indicator.value.style.left = left + 'px';
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -43,7 +61,7 @@ export default {
       context.emit('update:selected', title);
     };
     return {
-      defaults, titles, current, select
+      defaults, titles, current, select, navItems, indicator, container
     };
   }
 };
@@ -56,6 +74,7 @@ export default {
     display: flex;
     color: #333;
     border-bottom: 1px solid #d9d9d9;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -69,6 +88,16 @@ export default {
       &.selected {
         color: deeppink;
       }
+    }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
 
