ae859593271073d56e9490ca2c5e3a34c6ddfe03
wbs <1134954328@qq.com>
2021-08-15 19:39:54 +0800

    更新 SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,43 @@
 <template>
   <div>
-    <Switch v-model:value="bool"/>
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool"/>
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="demo">
+      <h2>支持 disabled</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool" disabled/>
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang='ts'>
 
 import Switch from '../lib/Switch.vue';
+import Button from '../lib/Button.vue';
 import {ref} from 'vue';
 
 export default {
-  components: {Switch},
+  components: {Switch, Button},
   setup() {
     const bool = ref(true);
     return {bool};
@@ -19,5 +46,33 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.demo {
+  border: 1px solid #d9d9d9;
+  margin: 16px 0 32px 0;
 
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid #d9d9d9;
+  }
+
+  &-component {
+    padding: 16px;
+  }
+
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed #d9d9d9;
+  }
+
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed #d9d9d9;
+
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+    }
+  }
+}
 </style>
