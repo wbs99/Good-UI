aa5654f79e06d80f2362ddea95c87bad2fe9bb09
wbs <1134954328@qq.com>
2021-08-15 16:46:56 +0800

    引入 github 样式

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    安装
-  </div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install good-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add good-ui</code></pre>
+
+    <p>
+      下一节：<a href="#/doc/start">开始使用</a>
+    </p>
+  </article>
 </template>
 
 <script lang='ts'>
