8e4b8fedd35879225f577d27e7abb6ab30fbc835
wbs <1134954328@qq.com>
2021-08-16 18:22:47 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git branch -M master
+git remote add origin git@github.com:wbs99/good-ui-website.git
+git push -f -u origin master &&
+cd ..
+echo https://wbs99.github.io/good-ui-website/index.html#/
